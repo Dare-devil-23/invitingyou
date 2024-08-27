ea60a64bf52a1dd092ff39392615ae1fe33ab137
Sahith K <sahithmummy22@gmail.com>
2024-08-27 22:09:34 +0530

    uploads

--- client/src/components/LoadImg.jsx
+++ client/src/components/LoadImg.jsx
@@ -3,7 +3,7 @@ import { getBaseUrl } from "../axios";
 export default function LoadImg({ src, ...rest }) {
   src = src && src.includes('https://')
     ? src
-    : `${getBaseUrl()}/images/` + src;
+    : `${getBaseUrl()}/uploads/` + src;
   return (
     <img {...rest} src={src} alt={''} />
   );
